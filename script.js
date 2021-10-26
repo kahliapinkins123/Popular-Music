68aa3e92e5fb804f8c5c43e2d13f8a3fb9f22b04
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-26 19:23:48 -0400

    Design work for album covers

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-const h2 = document.createElement('h2');
+document.addEventListener('DOMContentLoaded', ()=>{
+    let albumContainer = document.querySelector('#album-container');
 
-h2.textContent = "This was added by JS";
+    console.log(albumContainer);
 
-document.querySelector('body').appendChild(h2);
+    let li = document.createElement('li');
+
+    let h4 = document.createElement('h4');
+    let img = document.createElement('img');
+    let p = document.createElement('p');
+
+    li.textContent='Sasha Fierce'
+    //h4.textContent = "Sasha Fierce";
+    img.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Beyonc%C3%A9_-_4.png/220px-Beyonc%C3%A9_-_4.png';
+    p.textContent = "Artist: Beyonce";
+    //img.style.width = '300px';
+    //img.style.textAlign ='center';
+    //h2.style.textAlign = 'center';
+    //p.style.textAlign = 'center';
+
+    //li.appendChild(h4);
+    li.appendChild(img);
+    li.appendChild(p);
+    img.className = 'card-image';
+
+    li.className = 'card';
+
+    albumContainer.appendChild(li);
+})
