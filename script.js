24e4f0ee7bad6a5fd90278e7a487f353b5563f71
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-27 17:42:03 -0400

    Added first GET request from public API

--- script.js
+++ script.js
@@ -4,38 +4,66 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let fullList = document.querySelector('#full-list');
     let editorsChoice = document.querySelector('#editors-choice');
     let favorites = document.querySelector('#favorites');
+    let searchBtn = document.querySelector('#search-button');
     let heading = document.querySelector('.heading');
+    let input = document.querySelector('#search-input');
+
+    fetch("https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0", 
+    {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "shazam.p.rapidapi.com",
+		"x-rapidapi-key": "fe5de63a7bmsh41edb2a1c5603c6p15680fjsnf74c951202ad"
+	}}).then(resp => resp.json())
+    .then(obj=>{
+        tracks = obj['tracks']//[0]['images']['coverart']);
+        console.log(tracks)
+        createCards(tracks);
+    })
 
     editorsChoice.addEventListener('click',()=>{
         albumContainer.innerHTML = "";
         heading.textContent = "Editor's Choice";
 
         const editorsAlbums=[
-            {
-                album:'CTRL',
-                artist: 'SZA',
-                url:'https://upload.wikimedia.org/wikipedia/en/b/bf/SZA_-_Ctrl_cover.png'
-            },
-            {
-                album:'Thank Me Later',
-                artist: 'Drake',
-                url:'https://upload.wikimedia.org/wikipedia/en/9/9c/Drake_-_Thank_Me_Later_cover.jpg'
-            },
-            {
-                album:'Freudian',
-                artist: 'Daniel Caesar',
-                url: 'https://upload.wikimedia.org/wikipedia/en/b/b9/Freudian_by_Daniel_Caesar.jpg'
-            },
-            {
-                album:'H.E.R.',
-                artist: 'HER',
-                url: 'https://upload.wikimedia.org/wikipedia/en/4/4f/H.E.R._%282017%29_album_cover.jpg'
-            },
-            {
-                album:'Positions',
-                artist: 'Ariana Grande',
-                url: 'https://i.pinimg.com/originals/2f/60/0d/2f600dcdcdf907a363da3e22fb21a4ad.jpg'
-            }
+                {
+                    title:'CTRL',
+                    subtitle: 'SZA',
+                    images: {
+                        coverart:'https://upload.wikimedia.org/wikipedia/en/b/bf/SZA_-_Ctrl_cover.png'
+                    }
+                    
+                },
+                {
+                    title:'Thank Me Later',
+                    subtitle: 'Drake',
+                    images: {
+                        coverart: 'https://upload.wikimedia.org/wikipedia/en/9/9c/Drake_-_Thank_Me_Later_cover.jpg'
+                    }
+                },
+                {
+                    title:'Freudian',
+                    subtitle: 'Daniel Caesar',
+                    images: {
+                        coverart: 'https://upload.wikimedia.org/wikipedia/en/b/b9/Freudian_by_Daniel_Caesar.jpg'
+                    }
+                    
+                },
+                {
+                    title:'H.E.R.',
+                    subtitle: 'HER',
+                    images: {
+                        coverart: 'https://upload.wikimedia.org/wikipedia/en/4/4f/H.E.R._%282017%29_album_cover.jpg'
+                    }
+                },
+                {
+                    title:'Positions',
+                    subtitle: 'Ariana Grande',
+                    images: {
+                        coverart: 'https://i.pinimg.com/originals/2f/60/0d/2f600dcdcdf907a363da3e22fb21a4ad.jpg'
+                    }
+                }
+            
         ]
 
         createCards(editorsAlbums);
@@ -44,19 +72,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     testObjArray = [
         {
-            album: 'Dirty Computer',
-            artist: 'Janelle Monae',
-            url: 'https://www.udiscovermusic.com/wp-content/uploads/2015/10/Janelle-Mona%CC%81e-Dirty-Computer-.jpg'
+            title: 'Dirty Computer',
+            subtitle: 'Janelle Monae',
+            images: {
+                coverart: 'https://www.udiscovermusic.com/wp-content/uploads/2015/10/Janelle-Mona%CC%81e-Dirty-Computer-.jpg'
+            }
         },
         {
-            album: 'Xscape',
-            artist:'Michael Jackson',
-            url:'http://images6.fanpop.com/image/photos/37100000/new-album-mj-michael-jackson-37108862-1200-1200.jpg'
+            title: 'Xscape',
+            subtitle:'Michael Jackson',
+            images: {
+                coverart: 'http://images6.fanpop.com/image/photos/37100000/new-album-mj-michael-jackson-37108862-1200-1200.jpg'
+            }
         },
         {
-            album:'Legends Never Die',
-            artist: 'Juice WRLD',
-            url: 'https://cdn.vox-cdn.com/thumbor/qLSkOK78QQ9hON7SXwshppfC_Lc=/0x0:1600x1600/1400x1050/filters:focal(665x592:921x848):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/67039782/Juice_WRLD.0.jpg'
+            title:'Legends Never Die',
+            subtitle: 'Juice WRLD',
+            images: {
+                coverart: 'https://cdn.vox-cdn.com/thumbor/qLSkOK78QQ9hON7SXwshppfC_Lc=/0x0:1600x1600/1400x1050/filters:focal(665x592:921x848):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/67039782/Juice_WRLD.0.jpg'
+            }
         }   
     ]
 
@@ -71,7 +105,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
             
             
 
-            img.src = album.url;
+            img.src = album['images']['coverart'];
             faveBtn.textContent = 'Favorite';
             
             
@@ -103,7 +137,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
             })
 
             img.addEventListener('click',()=>{
-                alert(`This is ${album.artist}'s album ${album.album}.`);    
+                alert(`This is ${album['title']} by ${album['subtitle']}.`);    
             });
         }
 
@@ -131,25 +165,29 @@ document.addEventListener('DOMContentLoaded', ()=>{
     form.addEventListener('submit', function(e){
         e.preventDefault();
         
-        const album = document.querySelector('#album-name');
-        const artist = document.querySelector('#artist-name');
+        const title = document.querySelector('#album-name');
+        const subtitle = document.querySelector('#artist-name');
         const coverUrl = document.querySelector('#cover-url');
 
-        console.log(album.value);
-        console.log(artist.value);
-        console.log(coverUrl.value);
-
         const newAlbum = [{
-            album: `${album.value}`,
-            artist: `${artist.value}`,
-            url: `${coverUrl.value}`
+            title: `${title.value}`,
+            subtitle: `${artist.value}`,
+            images:{
+                coverart: `${coverUrl.value}`
+            } 
         }]
 
         createCards(newAlbum);
-        album.value ='';
-        artist.value ='';
+        title.value ='';
+        subtitle.value ='';
         coverUrl.value ='';
         
     })
+
+    searchBtn.addEventListener('click',e=>{
+        const input = document.querySelector('#search-input');
+        console.log(input.value);
+    });
+
     
 })
