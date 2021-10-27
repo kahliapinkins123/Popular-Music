5cb34086ae90724dbc70763e792223401fe780c9
Kahlia Pinkins <kahliapinkins123@gmail.com>
2021-10-27 19:20:31 -0400

    everything done, just needs cleanup

--- script.js
+++ script.js
@@ -7,19 +7,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let searchBtn = document.querySelector('#search-button');
     let heading = document.querySelector('.heading');
     let input = document.querySelector('#search-input');
+    let favoritesArray = [];
 
-    fetch("https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0", 
-    {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "shazam.p.rapidapi.com",
-		"x-rapidapi-key": "fe5de63a7bmsh41edb2a1c5603c6p15680fjsnf74c951202ad"
-	}}).then(resp => resp.json())
-    .then(obj=>{
-        tracks = obj['tracks']//[0]['images']['coverart']);
-        console.log(tracks)
-        createCards(tracks);
-    })
+    function fetchSongs(){
+        fetch("https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0", 
+        {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "shazam.p.rapidapi.com",
+            "x-rapidapi-key": "fe5de63a7bmsh41edb2a1c5603c6p15680fjsnf74c951202ad"
+        }}).then(resp => resp.json())
+        .then(obj=>{
+            tracks = obj['tracks']
+            createCards(tracks);
+        })
+    }
+
+    fetchSongs();
+    
+    
+    
 
     editorsChoice.addEventListener('click',()=>{
         albumContainer.innerHTML = "";
@@ -27,34 +34,33 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         const editorsAlbums=[
                 {
+                    title: 'Dirty Computer',
+                    subtitle: 'Janelle Monae',
+                    images: {
+                        coverart: 'https://www.udiscovermusic.com/wp-content/uploads/2015/10/Janelle-Mona%CC%81e-Dirty-Computer-.jpg'
+                    }
+                },
+                {
+                    title: 'Xscape',
+                    subtitle:'Michael Jackson',
+                    images: {
+                        coverart: 'http://images6.fanpop.com/image/photos/37100000/new-album-mj-michael-jackson-37108862-1200-1200.jpg'
+                    }
+                },
+                {
+                    title:'Legends Never Die',
+                    subtitle: 'Juice WRLD',
+                    images: {
+                        coverart: 'https://cdn.vox-cdn.com/thumbor/qLSkOK78QQ9hON7SXwshppfC_Lc=/0x0:1600x1600/1400x1050/filters:focal(665x592:921x848):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/67039782/Juice_WRLD.0.jpg'
+                    }
+                }, 
+                {
                     title:'CTRL',
                     subtitle: 'SZA',
                     images: {
                         coverart:'https://upload.wikimedia.org/wikipedia/en/b/bf/SZA_-_Ctrl_cover.png'
                     }
                     
-                },
-                {
-                    title:'Thank Me Later',
-                    subtitle: 'Drake',
-                    images: {
-                        coverart: 'https://upload.wikimedia.org/wikipedia/en/9/9c/Drake_-_Thank_Me_Later_cover.jpg'
-                    }
-                },
-                {
-                    title:'Freudian',
-                    subtitle: 'Daniel Caesar',
-                    images: {
-                        coverart: 'https://upload.wikimedia.org/wikipedia/en/b/b9/Freudian_by_Daniel_Caesar.jpg'
-                    }
-                    
-                },
-                {
-                    title:'H.E.R.',
-                    subtitle: 'HER',
-                    images: {
-                        coverart: 'https://upload.wikimedia.org/wikipedia/en/4/4f/H.E.R._%282017%29_album_cover.jpg'
-                    }
                 },
                 {
                     title:'Positions',
@@ -70,34 +76,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
   
     });
 
-    testObjArray = [
-        {
-            title: 'Dirty Computer',
-            subtitle: 'Janelle Monae',
-            images: {
-                coverart: 'https://www.udiscovermusic.com/wp-content/uploads/2015/10/Janelle-Mona%CC%81e-Dirty-Computer-.jpg'
-            }
-        },
-        {
-            title: 'Xscape',
-            subtitle:'Michael Jackson',
-            images: {
-                coverart: 'http://images6.fanpop.com/image/photos/37100000/new-album-mj-michael-jackson-37108862-1200-1200.jpg'
-            }
-        },
-        {
-            title:'Legends Never Die',
-            subtitle: 'Juice WRLD',
-            images: {
-                coverart: 'https://cdn.vox-cdn.com/thumbor/qLSkOK78QQ9hON7SXwshppfC_Lc=/0x0:1600x1600/1400x1050/filters:focal(665x592:921x848):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/67039782/Juice_WRLD.0.jpg'
-            }
-        }   
-    ]
+    
 
     
 
     //Create each card (will pass an object and replace Beyonce stuff w API stuff)
-    function createCards(albums=testObjArray){
+    function createCards(albums){
         for(const album of albums){
             let li = document.createElement('li');
             let img = document.createElement('img');
@@ -127,6 +111,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
                     faveBtn.style.background = 'black';
                     
                     //patch onto list of faves
+                    favoritesArray.push(album);
+                    console.log(favoritesArray);
                 }
                 else{
                     faveBtn.textContent = 'Favorite'
@@ -144,13 +130,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
 
 
-    createCards();
+    
 
     fullList.addEventListener('click',()=>{ 
         heading.textContent = "Full List";
         albumContainer.innerHTML = "";
 
-        createCards();
+        fetchSongs();
     });
 
     favorites.addEventListener('click',()=>{
@@ -160,6 +146,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
         //create new array of albums
         //if status is favorited, push onto array of albums
         //display array of albums
+        if(favoritesArray.length === 0){
+            let li = document.createElement('li');
+            li.textContent = "You haven't favorited anything yet!"
+            li.style.color = 'rgb(247, 55, 113)';
+            albumContainer.appendChild(li);
+        } else{
+            createCards(favoritesArray);
+
+        }
     });
 
     form.addEventListener('submit', function(e){
@@ -171,7 +166,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         const newAlbum = [{
             title: `${title.value}`,
-            subtitle: `${artist.value}`,
+            subtitle: `${subtitle.value}`,
             images:{
                 coverart: `${coverUrl.value}`
             } 
@@ -183,11 +178,5 @@ document.addEventListener('DOMContentLoaded', ()=>{
         coverUrl.value ='';
         
     })
-
-    searchBtn.addEventListener('click',e=>{
-        const input = document.querySelector('#search-input');
-        console.log(input.value);
-    });
-
     
 })
